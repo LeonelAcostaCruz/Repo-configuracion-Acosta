e30733fa9ef746c1b926883a25994109c92b7dbb
Leonel Acosta <al25220014@utcj.edu.mx>
2026-02-23 17:42:08 -0700

    Estructura inical del proyecto

---
+++ src/app.js
@@ -0,0 +1,7 @@
+
+const btn = document.getElementById("btn");
+const msg = document.getElementById("msg");
+
+btn.addEventListener("click", () => {
+    msg.textContent = "JS funcionando y repo listo 🚀";
+});
